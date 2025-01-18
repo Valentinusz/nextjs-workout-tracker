f4e0ca9b910631608c27ab242f1af19a356864f5
Valentinusz <68827048+Valentinusz@users.noreply.github.com>
2025-01-18 21:39:39 +0100

    update

--- src/app/workouts/[workoutId]/page.tsx
+++ src/app/workouts/[workoutId]/page.tsx
@@ -1,10 +1,22 @@
-import {Breadcrumbs, Title} from "@mantine/core";
+import {ActionIcon, Breadcrumbs, Button, Title} from "@mantine/core";
+import {IconLock, IconPlus} from "@tabler/icons-react";
 
 
 export default async function WorkoutDetailsPage({params}: {params: Promise<{ workoutId:number }>}) {
     const { workoutId } = await params;
+    const sets = [];
 
     return (
-        <Title>Workout #{workoutId}</Title>
+        <div className="flex flex-col gap-md">
+            <Title className="flex gap-sm items-center">Workout #{workoutId}
+                <Button className="ml-auto" leftSection={<IconPlus/>}>New set</Button>
+                <Button color="red" leftSection={<IconLock/>}>End workout</Button>
+            </Title>
+            <section>
+                <Title order={2}>
+                    Sets
+                </Title>
+            </section>
+        </div>
     )
 }
